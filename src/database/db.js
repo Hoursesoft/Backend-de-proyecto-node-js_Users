dc55a595ae9a0a608186ba81ba85c9761caa46eb
Breiner Saul Martinez Muñoz <177437192+Hoursesoft@users.noreply.github.com>
2025-05-23 15:54:23 -0500

    Se hizo la conexion con la db personal de breiner

--- src/database/db.js
+++ src/database/db.js
@@ -1,4 +1,9 @@
 module.exports = {
+    /*
     db: "mongodb+srv://ciclo3:ciclo3@cluster0.a6x9d.mongodb.net/utp_grupo2?retryWrites=true&w=majority"
-    //db: "mongodb://localhost:27017/utp_grupo2"
+    */
+
+    db: "mongodb+srv://myadmin:bd3138619952@cluster0.8ggs6q5.mongodb.net/Persona?retryWrites=true&w=majority"
+
 }
+
